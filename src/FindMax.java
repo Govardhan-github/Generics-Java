5c89b80d7136139d8a01e031033ec174cb9ff0a7
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-27 01:05:43 -0700

    ADDED : All 3 Methods InTo One Genric Method

--- src/FindMax.java
+++ src/FindMax.java
@@ -17,9 +17,13 @@ public class FindMax {
  /*
   * Declaring The Main Method 
   * And Printing The Maximum Integer Number
+  * And Printing The Maximum Float Number
+  * And Printing The Maximum String
   */
   public static void main(String args[]) {
-      System.out.println("Maximum Integer Is : " + max(Integer.valueOf(32), Integer.valueOf(56), Integer.valueOf(89)));
+      System.out.println("Maximum Integer Is : " + max(Integer.valueOf(32), Integer.valueOf(56), Integer.valueOf(89)));//To Print Maximum Integer
+      System.out.println("Maximum of 5.6 7.8 2.9 Float Number is : " + max(Float.valueOf( 5.6f), Float.valueOf(7.8f), Float.valueOf(2.9f)));//To Print Maximum Float Number
+      System.out.println("Maximum String Is : " + max("Govardhan", "Reddy" ,"Bajjuri"));//To Print Maximum String
 
   }
 }

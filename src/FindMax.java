2adcd32a3b5fea34d719fc81a870432dc72cac49
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-27 00:50:30 -0700

    ADDED : Finding Max String

--- src/FindMax.java
+++ src/FindMax.java
@@ -6,7 +6,7 @@ public class FindMax {
 
 // Determines The Largest Of Three Comparable Objects
  public static <T extends Comparable<T>> T max(T... elements) {
-	    T max = elements[0];
+	    T max = elements[2];
 	    for (T element : elements) {
 	        if (element.compareTo(max) > 0) {
 	            max = element;
@@ -16,10 +16,10 @@ public class FindMax {
 	}
  /*
   * Declaring The Main Method 
-  * And Printing The Maximum Number
+  * And Printing The Maximum String
   */
   public static void main(String args[]) {
-	     System.out.printf(" Max of 5.6 ,7.8 ,2.9 Float Number is : " + max(Float.valueOf( 5.6f), Float.valueOf(7.8f), Float.valueOf(2.9f)));
-	     
+	       System.out.println("Maximum String Is : " + max("Govardhan", "Reddy" ,"Bajjuri"));
+
   }
 }
